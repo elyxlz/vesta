43b8a9419cfdd7518683df5fcd0da1efe214f185
Emilio Pascarelli <emilio@pascarelli.com>
2026-04-09 01:17:52 +0100

    polish

--- app/src/components/ui/progress.tsx
+++ app/src/components/ui/progress.tsx
@@ -7,22 +7,21 @@ import { cn } from "@/lib/utils"
 
 function Progress({
   className,
-  indicatorClassName,
   value,
   ...props
-}: React.ComponentProps<typeof ProgressPrimitive.Root> & { indicatorClassName?: string }) {
+}: React.ComponentProps<typeof ProgressPrimitive.Root>) {
   return (
     <ProgressPrimitive.Root
       data-slot="progress"
       className={cn(
-        "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
+        "relative flex h-3 w-full items-center overflow-x-hidden rounded-full bg-muted",
         className
       )}
       {...props}
     >
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
-        className={cn("h-full w-full flex-1 bg-primary transition-all", indicatorClassName)}
+        className="size-full flex-1 bg-primary transition-all"
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
     </ProgressPrimitive.Root>
